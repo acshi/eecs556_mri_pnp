a0d2c44084087ef1ccd5994bef32a49d9aa8560e
ecummings0 <cumminge@umich.edu>
2021-04-14 14:46:48 -0400

    pogm example
    now includes example usage w/ pogm solver

--- testscript.jl
+++ testscript.jl
@@ -2,8 +2,8 @@
 
 using LinearAlgebra: norm
 using LinearMapsAA
-using MIRT: jim
-using Plots: plot
+using MIRT: jim, pogm_restart
+using Plots: plot, plot!
 
 include("utils.jl")
 include("load_fastmri_data.jl")
@@ -40,11 +40,22 @@ nrmse = (x) -> norm(x.-imref[:],2)/norm(imref,2)
 # recon data w/ admm algorithm
 # the primal function is still a little unstable. If you're testing another
 # denoiser, lmk, as I'd like to double check it once we have those ready
-x_hat,funout = admm((z,η,μ) -> primal(z,η,μ,A,B,y), (x) -> dncnn_denoise(Float32.(abs.(x))), 1, x0, niter=10, fun=(x,i)->nrmse(x))
-x_hat = reshape(x_hat,M,N)
+x_admm, nrmse_admm = admm((z,η,μ) -> primal(z,η,μ,A,B,y), (x) -> dncnn_denoise(Float32.(abs.(x))), 1, x0, niter=10, fun=(x,i)->nrmse(x))
+x_admm = reshape(x_admm,M,N)
 
-j3 = jim(abs.(x_hat[end:-1:1,end:-1:1]),title="admm recon")
-j4 = jim(abs.(imref[end:-1:1,end:-1:1]) - abs.(x_hat[end:-1:1,end:-1:1]),title="subtraction")
+j3 = jim(abs.(x_admm[end:-1:1,end:-1:1]),title="admm recon")
+j4 = jim(abs.(imref[end:-1:1,end:-1:1]) - abs.(x_admm[end:-1:1,end:-1:1]),title="admm sub")
 
-plot(j1,j2,j3,j4)
-plot(funout)
+x_pogm, nrmse_pogm = pogm_restart(x0[:], (x) -> undef, (x) -> A'*(A*x-y), 1,
+    g_prox=(x,c) -> dncnn_denoise(Float32.(abs.(reshape(x,M,N))))[:],
+    restart=:none,niter=10,fun=(aa,x,ac,ad) -> nrmse(x))
+
+x_pogm = reshape(x_pogm,M,N)
+
+j5 = jim(x_pogm[end:-1:1,end:-1:1],title="pogm recon")
+j6 = jim(abs.(imref[end:-1:1,end:-1:1]) - abs.(x_pogm[end:-1:1,end:-1:1]),title="pogm sub")
+
+plot(j1,j2,j3,j4,j5,j6)
+
+plot(0:10,nrmse_admm)
+plot!(0:10,nrmse_pogm)
